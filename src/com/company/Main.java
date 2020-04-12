3eaa09e992e11998b9f0ac5476fd5098e6172099
fengchuan <fc18840932606@163.com>
2020-04-12 22:52:58 +0800

    本地项目提交

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
-
 public class Main {
-
     public static void main(String[] args) {
-        // write your code here
+        //将一组int值视为unicode编码，然后将它们拼接成一个字符串;
+        int [] array={1,2,3,4,5};
+        array=new int []{1,2,3};
+
+        System.out.println(array.length);
     }
 }
